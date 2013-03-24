5e1546dbc85c3843a92d76062f95a964be9d85a9
savagecorp <savagecorp@gmail.com>
2013-03-24 16:25:11 -0400

    added js file

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
   
-  alert('hello there');
+  // alert('hello there');
   
-  // $("#slideshow > div:gt(0)").hide();
-  // 
-  //   setInterval(function() { 
-  //     $('#slideshow > div:first')
-  //       .fadeOut(1000)
-  //       .next()
-  //       .fadeIn(1000)
-  //       .end()
-  //       .appendTo('#slideshow');
-  //   },  3000);
-  //   
+  $("#slideshow > div:gt(0)").hide();
+  
+    setInterval(function() { 
+      $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+    },  3000);
+    
   
 });
